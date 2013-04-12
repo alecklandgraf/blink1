b459c4e8733d6615fed0e20efaa1d9799addcfb0
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2013-04-11 23:34:28 -0700

    stubs out color cycle

---
+++ color_cycle.py
@@ -0,0 +1,15 @@
+#!/usr/local/bin/python
+"""
+color_cycle.py
+"""
+from datetime import datetime
+from time import time
+
+def set_color():
+	pass
+
+def main():
+	pass
+
+if __name__ == "__main__":
+    main()
