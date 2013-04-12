fe08fd8f289c82d209698f3d0721064f4ab3ed8a
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2013-04-12 01:11:15 -0700

    adds christmas mode

--- color_cycle.py
+++ color_cycle.py
@@ -4,8 +4,10 @@ color_cycle.py
 """
 
 import time
-from subprocess import call
+import sys
 from random import randrange
+
+from blink1 import Blink1
 
 
 def get_color():
@@ -13,22 +15,34 @@ def get_color():
     return choices[randrange(len(choices))]
 
 
-def set_color(color='green'):
-    call(['lib/blink1-tool', '--%s' % color])
+def set_color(blink1_device, color='green'):
+    blink_color_setter = getattr(blink1_device, color)
+    blink_color_setter()
 
 
 def delay(default=5):
     time.sleep(default)
 
 
-def cycle():
+def cycle(blink1_device):
     color = get_color()
-    set_color(color)
+    set_color(blink1_device, color)
+
+
+def chrismas_mode(blink1_device):
+    while True:
+        set_color(blink1_device, 'green')
+        delay()
+        set_color(blink1_device, 'red')
+        delay()
 
 
 def main():
+    blinker = Blink1()
+    if 'christmas' in sys.argv:
+        chrismas_mode(blinker)
     while True:
-        cycle()
+        cycle(blinker)
         delay()
 
 if __name__ == "__main__":
