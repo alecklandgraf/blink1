2d25d90bb31c45dea68778fe85684d324d975943
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2013-04-12 00:45:10 -0700

    random works

--- blink1.py
+++ blink1.py
@@ -13,10 +13,15 @@ class Blink1():
     def blink(self, number_of_blinks, rgb_color=None):
         if rgb_color:
             self.rgb(rgb_color)
-            self._call_blink1_tool('--blink %s' % number_of_blinks)
+        self._call_blink1_tool('--blink %s' % number_of_blinks)
+
+    def random(self, number_of_blinks):
+        self._call_blink1_tool('--random', str(number_of_blinks))
 
     def rgb(self, rgb_color):
-        self._call_blink1_tool("--rgb %s" % rgb_color)
+        self._call_blink1_tool('--rgb %s' % rgb_color)
 
-    def _call_blink1_tool(self, args):
-        call([self.blink1_tool_file_path, args])
+    def _call_blink1_tool(self, *args):
+        call_list = list(args)
+        call_list.insert(0, self.blink1_tool_file_path)
+        call(call_list)
