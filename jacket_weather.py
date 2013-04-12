71e89ea43eee1e203067044a5f4f5088a864dc65
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2013-04-12 01:57:51 -0700

    PEP8, no tabs

---
+++ jacket_weather.py
@@ -0,0 +1,39 @@
+#!/usr/local/bin/python
+"""
+weather_blink.py
+
+blue if jacket weather
+red if not jacket weather
+
+uses http://openweathermap.org/ for FREE weather data!!
+"""
+
+from json import load
+from urllib2 import urlopen
+import time
+
+from blink1 import Blink1
+
+
+STATION = '2274'  # Portland, OR KPDX
+OPENWEATHERMAP_URL = 'http://api.openweathermap.org/data/2.1/weather/station/%s?type=json'
+FIVE_MIN = 60 * 5
+TEMP_THRESHOLD_F = 55
+
+
+def main():
+    blinker = Blink1()
+    while True:
+        data = urlopen(OPENWEATHERMAP_URL % STATION)
+        weather = load(data)
+        current_temp_F = (float(weather['main']['temp']) - 273.15) * 9/5 + 32
+
+        if current_temp_F > TEMP_THRESHOLD_F:
+            blinker.red()
+        else:
+            blinker.blue()
+        time.sleep(FIVE_MIN)
+
+
+if __name__ == "__main__":
+    main()
