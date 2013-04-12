5912ac2193dd3c69d0c390a51051daba2423d62e
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2013-04-11 23:55:46 -0700

    gets a basic demo

--- color_cycle.py
+++ color_cycle.py
@@ -2,14 +2,34 @@
 """
 color_cycle.py
 """
-from datetime import datetime
-from time import time
 
-def set_color():
-	pass
+from time import time
+from subprocess import call
+from random import randrange
+
+
+def get_color():
+    choices = ['red', 'green', 'blue', 'on']
+    return choices[randrange(len(choices))]
+
+
+def set_color(color='green'):
+    call(['../blink1-tool', '--%s' % color])
+
+
+def delay(default=5):
+    time.wait(default)
+
+
+def cycle():
+    color = get_color()
+    set_color(color)
+
 
 def main():
-	pass
+    while True:
+        delay()
+        cycle()
 
 if __name__ == "__main__":
     main()
