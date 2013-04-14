9bcf5f6c72acfeb0fd86a35d4dd605519a4116c0
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2013-04-14 16:00:03 -0700

    starts to implement rgb hex_color

--- blink1.py
+++ blink1.py
@@ -23,13 +23,21 @@ class Blink1():
 
     def rgb(self, rgb_color=None, hex_color=None):
         ''' rgb_color should be a tuple or list of strings or ints i.e. ('0xff', '0', '00') or [255,'0', '255]
-            hex_colorh should be a string i.e. '#FF0000'
+            hex_color should be a string i.e. '#FF0000'
         '''
         if rgb_color:
             if len(rgb_color) != 3:
                 raise Exception('rgb_color takes 3 arguments')
             rgb_string = '%s,%s,%s' % (rgb_color[0], rgb_color[1], rgb_color[2], )
             self._call_blink1_tool('--rgb', rgb_string)
+        if hex_color:
+            hex_color = str(hex_color)
+            hex_color = list(hex_color)
+            try:
+                hex_color.remove('#')
+            except ValueError:
+                pass
+
 
     def on(self):
         self._call_blink1_tool('--on')
