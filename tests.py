7fc72cf2a576a8e782761f7dc8c4a71e8ab50815
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2013-04-14 13:05:31 -0700

    stills fails on rgb

--- tests.py
+++ tests.py
@@ -6,10 +6,14 @@ tests.py
 from blink1 import Blink1
 
 
+def rgb_test(blink_device):
+    blink_device.rgb(rgb_color=(255, 0, 0))
+
+
 def main():
-    if '--loop' in sys.argv:
-        jacket_weather_loop()
-    jacket_weather()
+    blinker = Blink1()
+    rgb_test(blinker)
+
 
 if __name__ == "__main__":
     main()
