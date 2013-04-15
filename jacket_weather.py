6a15451d69a8d02feb588af8c27c3ee1b3bef402
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2013-04-15 14:21:48 -0700

    gets weather

--- jacket_weather.py
+++ jacket_weather.py
@@ -18,7 +18,9 @@ from blink1 import Blink1
 
 
 STATION = '2274'  # Portland, OR KPDX
-OPENWEATHERMAP_URL = 'http://api.openweathermap.org/data/2.1/weather/station/%s?type=json'
+CITY = '5746545'  # Portland
+OPENWEATHERMAP_URL = 'http://api.openweathermap.org/data/2.1/weather/city/%s?type=json'
+OPENWEATHERMAP_FORECAST_URL = 'http://api.openweathermap.org/data/2.1/forecast/city/%s?type=json'
 FIVE_MIN = 60 * 5
 TEN_MIN = 60 * 10
 TEMP_THRESHOLD_F = 50
@@ -27,19 +29,21 @@ TEMP_THRESHOLD_F = 50
 def jacket_weather():
     blinker = Blink1()
     try:
-        data = urlopen(OPENWEATHERMAP_URL % STATION)
+        data = urlopen(OPENWEATHERMAP_URL % CITY)
         weather = load(data)
         current_temp_F = (float(weather['main']['temp']) - 273.15) * 9 / 5 + 32
+        current_condition = weather['weather'][0]['main']
     except:
         print "Error: couldn't get weather data"
-        current_temp_F = 50
+        current_temp_F = "unknown"
+        current_condition = "unknown"
         blinker.blink(5)
 
     if current_temp_F > TEMP_THRESHOLD_F:
         blinker.red()
     else:
         blinker.blue()
-    print "Current weather for your location is %s degrees F @ %s" % (current_temp_F, strftime("%Y-%m-%d %H:%M:%S", time.localtime()), )
+    print "Current weather for your location is %s degrees F and %s @ %s" % (current_temp_F, current_condition, strftime("%Y-%m-%d %H:%M:%S", time.localtime()), )
 
 
 def jacket_weather_loop():
