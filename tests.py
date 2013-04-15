b7df5157584e85fd62fff65beecbf97d0353b8af
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2013-04-14 18:39:04 -0700

    all tests pass with captured output!

--- tests.py
+++ tests.py
@@ -8,22 +8,30 @@ from blink1 import Blink1
 
 def rgb_test(blink_device):
     blink_device.rgb(rgb_color=(255, 0, 0))
-    print "return code", blink_device.command_output
+    assert "set dev:0 to rgb:0xff,0x00,0x00 over 300 msec" in blink_device.command_output
     blink_device.delay(1)
     blink_device.rgb(rgb_color=(0, 255, 0))
+    assert "set dev:0 to rgb:0x00,0xff,0x00 over 300 msec" in blink_device.command_output
     blink_device.delay(1)
     blink_device.rgb((0, 0, 255))
+    assert "set dev:0 to rgb:0x00,0x00,0xff over 300 msec" in blink_device.command_output
     blink_device.delay(1)
     blink_device.rgb()
+    assert "set dev:0 to rgb:0x00,0x00,0xff over 300 msec" in blink_device.command_output
     blink_device.rgb(hex_color='#ff0000')
+    assert "set dev:0 to rgb:0xff,0x00,0x00 over 300 msec" in blink_device.command_output
     blink_device.delay(1)
     blink_device.rgb(hex_color='#1072B9')
+    assert "set dev:0 to rgb:0x10,0x72,0xb9 over 300 msec" in blink_device.command_output
     blink_device.delay(1)
     blink_device.rgb(hex_color='#959595')
+    assert "set dev:0 to rgb:0x95,0x95,0x95 over 300 msec" in blink_device.command_output
     blink_device.delay(1)
     blink_device.rgb(hex_color='#8BC542')
+    assert "set dev:0 to rgb:0x8b,0xc5,0x42 over 300 msec" in blink_device.command_output
     blink_device.delay(1)
     blink_device.rgb(hex_color='1072B9')
+    assert "set dev:0 to rgb:0x10,0x72,0xb9 over 300 msec" in blink_device.command_output
 
 
 def _hex_to_rgb_test(blink_device):
